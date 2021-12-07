c729975da0beeaafa4bc55ced50c3ba5338ccf04
Tycoon19 <kevin960831@naver.com>
2021-12-07 13:22:07 +0900

    방 상세정보 데이터를 통째로 넘겨서 반영

--- app/src/main/java/com/example/landapp_jaewhi/datas/Room.kt
+++ app/src/main/java/com/example/landapp_jaewhi/datas/Room.kt
@@ -30,13 +30,13 @@ class Room(
     fun getFormattedFloor() : String {
 
         if(this.floor > 0){
-            return ", ${this.floor}층"
+            return " ${this.floor}층"
         }
         else if (this.floor == 0){
-            return ", 반지하"
+            return " 반지하"
         }
         else{
-            return ", 지하${-this.floor}층"
+            return " 지하${-this.floor}층"
         }
 
     }
